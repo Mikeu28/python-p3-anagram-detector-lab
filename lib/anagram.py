77189c356efe81220a49d682c66cb436dd86de4a
Michael <mikeurezzio@gmail.com>
2023-07-11 18:07:45 -0400

    completed anagram lab, freelo

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+
+class Anagram:
+    
+    def __init__( self, given_word ):
+        self.word = sorted( given_word )
+        #print ( self.word )
+
+    def match( self, word_match ):
+        word_list = []
+        for word in word_match:
+            if ( sorted( word ) == self.word):
+                word_list.append( word )
+        return word_list
+
+        
+
+#a1 = Anagram("Hello")
